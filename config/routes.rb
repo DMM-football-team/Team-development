44b781bc4596544891a424e2f679d4449afb66cc
tanayuuuuu <101308377+tanayuuuuu@users.noreply.github.com>
2022-06-17 05:09:14 +0000

    管理者用コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,35 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'order_details/update'
+  end
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'items/new'
+    get 'items/index'
+    get 'items/show'
+    get 'items/create'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
