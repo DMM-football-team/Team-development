f9657accc573abc1c175cf41deccc0988bd722b0
tanayuuuuu <101308377+tanayuuuuu@users.noreply.github.com>
2022-06-18 15:18:13 +0000

    とりあえずのコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
+  devise_for :customers, controllers: {
   sessions:      'customers/sessions',
   passwords:     'customers/passwords',
   registrations: 'customers/registrations'
