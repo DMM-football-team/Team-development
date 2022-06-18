1eecefc968e8b0c9665e16bb082c67d61c06bdc0
EC2 Default User <ec2-user@ip-172-31-94-226.ec2.internal>
2022-06-18 01:54:34 +0000

    pdate

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,19 @@
 class Admin::OrdersController < ApplicationController
   def index
+    def index
+  		@orders = Order.all
+    end
+
+    def current_index
+      @orders = Order.where(customer_id: params[:id]).page(params[:page]).per(10)
+      render :index
+    end
+
+    def today_order_index
+      now = Time.current
+      @orders = Order.where(created_at: now.all_day).page(params[:page]).per(10)
+      render :index
+    end
   end
 
   def show
