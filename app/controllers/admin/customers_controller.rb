4b428078893de94d4c01ad883e3ce05e1ce6025d
KOHEI FUKUSHIMA <pecpec1129@gmail.com>
2022-06-22 06:52:45 +0000

    update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,11 +4,20 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :post_code, :address, :is_deleted, :email)
   end
 end
