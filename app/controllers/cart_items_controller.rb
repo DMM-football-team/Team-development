87050d7ad7bf4c20087860ffa9d3cc84bdd8e1bc
tanayuuuuu <101308377+tanayuuuuu@users.noreply.github.com>
2022-06-23 08:16:35 +0000

    kkk

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,16 +1,27 @@
 class CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
   end
 
   def update
   end
 
   def create
+    @cart_item = CartItem.new
+    @cart_item.save
+    redirect_to cart_items_path
+
   end
 
   def destroy
   end
 
   def all_destroy
+  end
+
+  private
+
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
