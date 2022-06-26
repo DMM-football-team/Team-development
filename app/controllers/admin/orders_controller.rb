8059f1cf400c28c1e334b84162502cd0816aac6b
EC2 Default User <ec2-user@ip-172-31-94-226.ec2.internal>
2022-06-26 10:54:12 +0000

    update

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,18 +3,6 @@ class Admin::OrdersController < ApplicationController
   		@orders = Order.all
   end
 
-  def current_index
-    @orders = Order.where(customer_id: params[:id]).page(params[:page]).per(10)
-    render :index
-  end
-
-  def today_order_index
-    now = Time.current
-    @orders = Order.where(created_at: now.all_day).page(params[:page]).per(10)
-    render :index
-
-  end
-
   def show
     @order = Order.find(params[:id])
 		@order_details = @order.order_details
